45d905d87528f8a034c3d3ffc277655c38a8403e
Thejan K.G.T <99307599+IT21222740@users.noreply.github.com>
2024-03-20 00:14:49 +0530

    Database configured

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,21 +1,22 @@
+const asyncHandler = require("express-async-handler")
 //@desc Get all course
 //@route GET /api/courses
 //@access public
-const getCourses = async (req,res)=>{
+const getCourses = asyncHandler( (req,res)=>{
     res.status(200).json({message:"Get All courses"})
-}
+});
 
 //@desc Get a course
 //@route Get /api/courses/:id
 //@access public
-const getCourse = async (req, res) => {
+const getCourse = asyncHandler( (req, res) => {
     res.status(200).json({ message:`Get the course with id ${req.params.id}`})
- }
+ });
 
 //@desc Post a course
 //@route Post /api/courses
 //@access public
-const publishCourse = async (req, res) => {
+const publishCourse = asyncHandler( (req, res) => {
    console.log("The request body",req.body)
    const {name,code,credits} = req.body
    if(!name || !code || !credits){
@@ -23,19 +24,20 @@ const publishCourse = async (req, res) => {
       throw new Error("All fiels are mandatory")
    }
     res.status(201).json({ message:"Create a course"})
- }
+ });
 
 //@desc Update a course
 //@route Put /api/courses/:id
 //@access public
- const updateCourse = async (req, res) => {
+ const updateCourse = asyncHandler( (req, res) => {
     res.status(200).json({ message:`Update the course with id ${req.params.id}`})
- } 
+ } );
 
 //@desc Delete a course
 //@route Delete /api/course/:id
 //@access public
- const deleteCourese = async (req, res) => {
+ const deleteCourese = asyncHandler( (req, res) => {
     res.status(200).json({ message:`Delete the course with id ${req.params.id}`})
- }
+ });
+ 
 module.exports = {getCourses,getCourse,publishCourse,updateCourse,deleteCourese}
