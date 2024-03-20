d93b5e4e99c3ea4d451a277a73441a8240725726
Thejan K.G.T <99307599+IT21222740@users.noreply.github.com>
2024-03-20 21:01:39 +0530

    Registrtion and login completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,67 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 //@desc Register a user
 //@route Post /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register the user" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandotary");
+  }
+  const userAvailable = await User.findOne({ email: email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered!");
+  }
+  //HashPassword
+  const hashPassword = await bcrypt.hash(password, 10);
+  console.log("hash Password", hashPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashPassword,
+  });
+  if (user) {
+    res.status(201).json({ _id: user._id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
 });
 
 //@desc Login a user
 //@route Post /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+
+  const user = await User.findOne({ email: email });
+  //compare password with hashpassword
+  if (user && bcrypt.compare(password, user.password)) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user._id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or password is not valid");
+  }
 });
 
 //@desc current userInfo
